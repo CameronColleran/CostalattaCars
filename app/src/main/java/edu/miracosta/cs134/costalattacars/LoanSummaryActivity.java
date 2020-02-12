0985683e0a2f886d5242da0442fcc8bf712e9ea5
Cameron <cameroncolleran3@gmail.com>
2020-02-12 15:44:12 -0800

    V2

--- app/src/main/java/edu/miracosta/cs134/costalattacars/LoanSummaryActivity.java
+++ app/src/main/java/edu/miracosta/cs134/costalattacars/LoanSummaryActivity.java
@@ -5,17 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.text.NumberFormat;
 import java.util.Locale;
 
 import edu.miracosta.cs134.costalattacars.model.CarLoan;
 
-public class LoanSummaryActivity extends AppCompatActivity {
+public class LoanSummaryActivity extends AppCompatActivity
+{
 
     // Instance variable
     CarLoan carLoan;
     private NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.getDefault());
+    NumberFormat percent = NumberFormat.getPercentInstance(Locale.getDefault());
+
+    private TextView monthlyPaymentLoanTextView;
+    private TextView carPriceTextView;
+    private TextView salesTaxRateTextView;
+    private TextView taxAmountTextView;
+    private TextView downPaymentTextView;
+    private TextView totalCostTextView;
+    private TextView borrowedAmountTextView;
+    private TextView interestAmountTextVIew;
+    private TextView loanTermTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,14 +52,39 @@ public class LoanSummaryActivity extends AppCompatActivity {
         carLoan.setDownPayment(downPayment);
         carLoan.setLoanTerm(loanTerm);
 
-        // Populate text views with the data from the carLoan model object
+        // Wiring up local text views to activity text views
+        monthlyPaymentLoanTextView = findViewById(R.id.monthlyPaymentLoanTextView);
+        carPriceTextView = findViewById(R.id.carPriceTextView);
+        salesTaxRateTextView = findViewById(R.id.salesTaxRateTextView);
+        taxAmountTextView = findViewById(R.id.taxAmountTextView);
+        downPaymentTextView = findViewById(R.id.downPaymentTextView);
+        totalCostTextView = findViewById(R.id.totalCostTextView);
+        borrowedAmountTextView = findViewById(R.id.borrowedAmountTextView);
+        interestAmountTextVIew = findViewById(R.id.interestAmountTextView);
+        loanTermTextView = findViewById(R.id.loanTermTextView);
 
+        // Populate text views with the data from the carLoan model object
+        calculatePayments();
     }
 
     // Return to main activity when return button is clicked
     public void returnToMain(View v)
     {
         finish();
+    }
+
+    // Setting text views to model data
+    public void calculatePayments()
+    {
+        monthlyPaymentLoanTextView.setText(currency.format(carLoan.monthlyPayment()));
+        carPriceTextView.setText(currency.format(carLoan.getPrice()));
+        salesTaxRateTextView.setText(percent.format(carLoan.OCEANSIDE_TAX_RATE));
+        taxAmountTextView.setText(currency.format(carLoan.taxAmount()));
+        downPaymentTextView.setText(currency.format(carLoan.getDownPayment()));
+        totalCostTextView.setText(currency.format(carLoan.totalCost()));
+        borrowedAmountTextView.setText(currency.format(carLoan.borrowedAmount()));
+        interestAmountTextVIew.setText(currency.format(carLoan.interestAmount()));
+        loanTermTextView.setText(currency.format(carLoan.getLoanTerm()));
     }
 
 
